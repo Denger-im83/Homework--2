90a467bd41f6a42c96d9c2b79b457cfc671ceb4e
Denis <denis_im@mail.ru>
2023-04-16 21:12:31 +0500

    определение количества элементов в числе

---
+++ Task 1/Test/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+int Get(int num)
+{
+    int count=1;
+    while (num>9)
+    {
+       // num=num/10;
+        num/=10;
+        count++;
+
+    }
+    return count;
+
+
+}
+int a=int.Parse(Console.ReadLine());
+Console.WriteLine("количество элементов в числе :"+ Get(a));
