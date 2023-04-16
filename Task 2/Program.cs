eb91f14ad52d1d368137ee7826264aa81412e432
Denis <denis_im@mail.ru>
2023-04-16 17:27:35 +0500

    определение значение третьего  числа

---
+++ Task 2/Program.cs
@@ -0,0 +1,35 @@
+﻿int CheckNumber(int test)
+{
+    if (test < 0)
+    {
+        return test * -1;
+
+    }
+    else
+    { return test; }
+}
+bool CheckLength(int length)
+{
+    if (length > 2)
+    {
+        return true;
+    }
+    else
+    { return false; }
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+number = CheckNumber(number);
+
+string number_in_text = number.ToString();
+
+bool more_than_two_symbols = CheckLength(number_in_text.Length);
+
+if (more_than_two_symbols == true)
+{
+    Console.WriteLine("Третья цифра в введенной цифре: " + number_in_text[2]);
+}
+else
+    Console.WriteLine(" => Третьей цифры нет");
