7683531d6600de5aa31ae8ee26b5a91e05b7373e
Denis <denis_im@mail.ru>
2023-04-16 17:43:19 +0500

    определение дня недели

---
+++ Task 3/Program.cs
@@ -0,0 +1,32 @@
+﻿string CheckWeekdays(int weekdays)
+{
+    if (weekdays == 6 || weekdays == 7)
+    {
+        return "Да, это выходной";
+    }
+    else
+    { 
+        return "Нет, это не выходной";
+    }
+}
+bool CheckNumber(int number)
+{
+    if (number <= 7 && number > 0 )
+    {
+        return true;
+    }
+    else
+    { return false; }
+}
+
+Console.Write("Введите число: ");
+int days_in_week = int.Parse(Console.ReadLine());
+
+if (CheckNumber(days_in_week))
+{
+    Console.WriteLine(CheckWeekdays(days_in_week));
+}
+else
+    Console.WriteLine("Неверно введенное число ");
+
+
